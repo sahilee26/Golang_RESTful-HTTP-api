ef66de279c96c39220efbc3c26b1fb620c1fc816
Sahil Aggarwal <31005698+sahilee26@users.noreply.github.com>
2020-05-26 17:30:32 +0530

    Update http-api.go

--- http-api.go
+++ http-api.go
@@ -34,6 +34,8 @@ func main() {
 
 	router.HandleFunc("/posts/{id}", getPost).Methods("GET")
 
+	router.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
+
 	http.ListenAndServe(":5000", router)
 }
 
@@ -53,6 +55,7 @@ func getPost(w http.ResponseWriter, r *http.Request) {
 	if id >= len(posts) {
 		w.WriteHeader(404)
 		w.Write([]byte("No post found with specified ID"))
+		return
 	}
 
 	post := posts[id]
@@ -77,4 +80,30 @@ func addItem(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "application/json")
 
 	json.NewEncoder(w).Encode(posts)
+}
+
+func updatePost(w http.ResponseWriter, r *http.Request) {
+	var idParam string = mux.Vars(r)["id"]
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		// There was an error
+		w.WriteHeader(400)
+		w.Write([]byte("ID could not be converted to interger"))
+		return
+	}
+
+	//error checking
+	if id >= len(posts) {
+		w.WriteHeader(404)
+		w.Write([]byte("No post found with specified ID"))
+		return
+	}
+
+	var updatedPost Post
+	json.NewDecoder(r.Body).Decode(&updatedPost)
+
+	posts[id] = updatedPost
+
+	w.Header().Set("Content-Tvpe", "application/json")
+	json.NewEncoder(w).Encode(updatedPost)
 }
