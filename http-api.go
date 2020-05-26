959ae41e3be84f1ca199e6752d53477959bd6d04
Sahil Aggarwal <31005698+sahilee26@users.noreply.github.com>
2020-05-26 17:10:29 +0530

    Update http-api.go

--- http-api.go
+++ http-api.go
@@ -1,9 +1,9 @@
-
 package main
 
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -28,9 +28,43 @@ var posts []Post = []Post{}
 func main() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/add", addItem).Methods("POST")
+	router.HandleFunc("/posts", addItem).Methods("POST")
+
+	router.HandleFunc("/posts", getAllPosts).Methods("GET")
+
+	router.HandleFunc("/posts/{id}", getPost).Methods("GET")
 
 	http.ListenAndServe(":5000", router)
+}
+
+func getPost(w http.ResponseWriter, r *http.Request) {
+
+	//get the ID of the post from the route parameter
+	var idParam string = mux.Vars(r)["id"]
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		// There was an error
+		w.WriteHeader(400)
+		w.Write([]byte("ID could not be converted to interger"))
+		return
+	}
+
+	//error checking
+	if id >= len(posts) {
+		w.WriteHeader(404)
+		w.Write([]byte("No post found with specified ID"))
+	}
+
+	post := posts[id]
+
+	w.Header().Set("Content-Tvpe", "application/json")
+	json.NewEncoder(w).Encode(post)
+}
+
+func getAllPosts(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-type", "application/json")
+	json.NewEncoder(w).Encode(posts)
 }
 
 func addItem(w http.ResponseWriter, r *http.Request) {
