d355a7587ff2f76b1b446e4e588ec87110b5a363
Sahil Aggarwal <31005698+sahilee26@users.noreply.github.com>
2020-05-26 10:40:54 +0530

    Create JSON-object.go

---
+++ learning/JSON-object.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/test", test)
+
+	http.ListenAndServe(":5000", router)
+}
+
+func test(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	json.NewEncoder(w).Encode(struct {
+		ID string
+	}{"555"})
+}
